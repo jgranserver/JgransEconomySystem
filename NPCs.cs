a306cc9a6ceaa262cc877bb0534e9b2c8ab8ab43
jgranserver <jgranserver@gmail.com>
2025-04-16 20:45:57 +0800

    Refactor NPCType class for improved code formatting and consistency

--- NPCs.cs
+++ NPCs.cs
@@ -2,18 +2,18 @@ using Terraria.ID;
 
 namespace JgransEconomySystem
 {
-	public static class NPCType
-	{
-		static JgransEconomySystemConfig config = new JgransEconomySystemConfig();
+    public static class NPCType
+    {
+        static JgransEconomySystemConfig config = new JgransEconomySystemConfig();
 
-		public static bool IsHostile(int npcType) => config.HostileNPCs.Value.Contains(npcType);
+        public static bool IsHostile(int npcType) => config.HostileNPCs.Value.Contains(npcType);
 
-		public static bool IsSpecial(int npcType) => config.SpecialNPCs.Value.Contains(npcType);
+        public static bool IsSpecial(int npcType) => config.SpecialNPCs.Value.Contains(npcType);
 
-		public static bool IsBoss1(int npcType) => config.BossNPCs1.Value.Contains(npcType);
+        public static bool IsBoss1(int npcType) => config.BossNPCs1.Value.Contains(npcType);
 
-		public static bool IsBoss2(int npcType) => config.BossNPCs2.Value.Contains(npcType);
+        public static bool IsBoss2(int npcType) => config.BossNPCs2.Value.Contains(npcType);
 
-		public static bool IsBoss3(int npcType) => config.BossNPCs3.Value.Contains(npcType);
-	}
+        public static bool IsBoss3(int npcType) => config.BossNPCs3.Value.Contains(npcType);
+    }
 }
